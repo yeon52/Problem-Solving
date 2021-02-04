0ebff6d71e15af14bdcf7ae8017dd2c9b78d2639
Ga-Yeon Park <rkdusdd52@naver.com>
2021-02-05 02:47:01 +0900

    dfs,bfs

--- CodingTest/DFS&BFS/baekjoon/4963.py
+++ CodingTest/DFS&BFS/baekjoon/4963.py
@@ -31,3 +31,45 @@ while True:
 
 for i in result:
     print(i)
+
+
+
+#### bfs 이용 #####
+# from collections import deque
+# def bfs(m, x, y):
+#     if x<0 or y<0 or x>h-1 or y>w-1 or m[x][y]==0:
+#         return False
+#     queue = deque()
+#     queue.append((x, y))
+#     dx = [1, -1, 0, 0, 1, -1, 1, -1]
+#     dy = [0, 0, 1, -1, 1, -1, -1, 1]
+# 
+#     while queue:
+#         x, y = queue.popleft()
+#         for i in range(8):
+#             nx = x + dx[i]
+#             ny = y + dy[i]
+#             if nx < 0 or ny < 0 or nx > h - 1 or ny > w - 1:
+#                 continue
+#             elif m[nx][ny] == 1:
+#                 m[nx][ny] = 0
+#                 queue.append((nx,ny))
+#     return True
+# 
+# result = []
+# while True:
+#     w, h = map(int, input().split())
+#     if w == 0 and h == 0:
+#         break
+#     m = []
+#     cnt = 0
+#     for i in range(h):
+#         m.append(list(map(int, input().split())))
+#     for i in range(h):
+#         for j in range(w):
+#             if bfs(m, i, j):
+#                 cnt += 1
+#     result.append(cnt)
+# 
+# for i in result:
+#     print(i)
