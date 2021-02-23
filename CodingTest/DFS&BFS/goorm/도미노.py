84e0ec6b5fc48c3c83e2a552e877eeb2f25246c4
Ga-Yeon Park <rkdusdd52@naver.com>
2021-02-24 01:59:08 +0900

    ?

--- CodingTest/DFS&BFS/goorm/도미노.py
+++ CodingTest/DFS&BFS/goorm/도미노.py
@@ -1,8 +1,15 @@
 a, b, c = map(int, input().split())
-domino = []
+domino = [[] for _ in range(a+1)]
 
 for i in range(b):
 	d,e = map(int, input().split())
-	domino.append((d,e))
+	domino[d].append(e)
 
 hand = int(input())
+cnt = 1
+
+for i in range(hand, a+1):
+	if domino[i]:
+		cnt+=1
+
+print(cnt)
