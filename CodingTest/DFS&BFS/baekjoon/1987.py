91c09a4798673a8af1b14e919563c902e6d75b1c
Ga-Yeon Park <rkdusdd52@naver.com>
2021-03-08 20:11:34 +0900

    dfs

--- CodingTest/DFS&BFS/baekjoon/1987.py
+++ CodingTest/DFS&BFS/baekjoon/1987.py
@@ -1,3 +1,4 @@
+# 알파벳
 import sys
 input = sys.stdin.readline
 
@@ -8,7 +9,9 @@ dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
 global max_cnt
 max_cnt = 1
-def dfs(x,y, cnt):
+
+
+def dfs(x, y, cnt):
     global max_cnt
     visited[ord(graph[x][y])-65] = True
     if cnt > max_cnt:
@@ -19,12 +22,13 @@ def dfs(x,y, cnt):
         if nx < 0 or ny < 0 or nx > R - 1 or ny > C - 1:
             continue
         if not visited[ord(graph[nx][ny])-65]:
-            dfs(nx,ny,cnt+1)
+            dfs(nx, ny, cnt+1)
             visited[ord(graph[nx][ny])-65] = False
+
 
 for i in range(R):
     graph.append(list(input().rstrip()))
 
-dfs(0,0,max_cnt)
+dfs(0, 0, max_cnt)
 
 print(max_cnt)
