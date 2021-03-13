bb0737f4ee8b342c08a7fd1989d9b36f216bf8c8
Ga-Yeon Park <rkdusdd52@naver.com>
2021-03-13 13:16:34 +0900

    rename

--- CodingTest/Eratosthenes/1929.py
+++ CodingTest/Eratosthenes/1929.py
@@ -4,16 +4,16 @@ M, N = map(int, input().split())
 
 is_prime = [True]*(N+1)
 
-for i in range(2,int(math.sqrt(N))+1):
-    j=2
+for i in range(2, int(math.sqrt(N))+1):
+    j = 2
     if is_prime[i]:
         while i*j <= N:
             is_prime[i*j] = False
             j += 1
 
 cnt = 0
-for i in range(M,N+1):
-    if is_prime[i] and i>1:
+for i in range(M, N+1):
+    if is_prime[i] and i > 1:
         print(i)
 
 # 소수판별
