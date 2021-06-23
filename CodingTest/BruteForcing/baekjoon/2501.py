8d6e2c4da5587137ffc59db097f4fac893ba73f1
Ga-Yeon Park <rkdusdd52@naver.com>
2021-06-23 17:11:03 +0900

    약수구하기

--- CodingTest/BruteForcing/baekjoon/2501.py
+++ CodingTest/BruteForcing/baekjoon/2501.py
@@ -1,15 +1,10 @@
-from collections import deque
-
-T = int(input())
-binary = deque()
-
-for i in range(T):
-    n = int(input())
-    while n!=0:
-        binary.append(n%2)
-        n=n//2
-
-    for i in range(len(binary)):
-        if binary.popleft() == 1:
-            print(i,end=' ')
-    print()
+N, K = map(int, input().split(' '))
+cnt = 0
+ans = 0
+for i in range(1, N+1):
+    if N % i == 0:
+        cnt += 1
+    if cnt == K:
+        ans = i
+        break
+print(ans)
