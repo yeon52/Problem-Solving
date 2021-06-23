90bb74ab424da4da32515a57a5b9841642bbe705
Ga-Yeon Park <rkdusdd52@naver.com>
2021-06-23 16:24:22 +0900

    부캠 자가진단

--- CodingTest/Implementation/boosttest.py
+++ CodingTest/Implementation/boosttest.py
@@ -4,40 +4,16 @@ import sys
 
 input = sys.stdin.readline
 
-def countOf(n):
-    return arr.count(n)
-
 arr = list(map(int, input().split()))
-arr.sort()
-arr2 = set(arr)
-arr2 = list(arr2)
+arr_set = set(arr)
 result = []
-for i in arr2:
-    t = countOf(i)
-    if t>1:
-        result.append(t)
+for i in arr_set:
+    cnt = arr.count(i)
+    if cnt > 1:
+        result.append(cnt)
 
 if len(result)==0:
     print(-1)
 else:
     print(result)
 
-#다른풀이
-# pre = arr[0]
-# result=[]
-# cnt = 1
-# for i in arr[1:]:
-#     if pre == i:
-#         cnt += 1
-#     elif cnt > 1:
-#         result.append(cnt)
-#         cnt = 1
-#     pre = i
-#
-# if cnt>1:
-#     result.append(cnt)
-#
-# if len(result)==0:
-#     print(-1)
-# else:
-#     print(result)
